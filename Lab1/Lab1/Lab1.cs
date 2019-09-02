1433191b6bea625227bcc1eb8cd45a09162e8d02
OpticalSpring <cosmos5477@gmail.com>
2019-09-02 23:15:44 +0900

    lab1

---
+++ Lab1/Lab1/Lab1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    public static class Lab1
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
