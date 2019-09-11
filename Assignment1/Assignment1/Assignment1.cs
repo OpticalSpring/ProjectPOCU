569e4103cef423e621db009a9cbff392c0d697b2
OpticalSpring <cosmos5477@gmail.com>
2019-09-11 23:22:56 +0900

    55

---
+++ Assignment1/Assignment1/Assignment1.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+
+namespace Assignment1
+{
+    public static class Assignment1
+    {
+        public static void PrintIntegers(StreamReader input, StreamWriter output)
+        {
+            int[] str = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                str[i] = int.Parse(input.ReadLine());
+            }
+            string str2 = "oct";
+            str2 = str2.PadLeft(12);
+            output.Write(str2);
+            str2 = "dec";
+            str2 = str2.PadLeft(11);
+            output.Write(str2);
+            str2 = "hex";
+            str2 = str2.PadLeft(9);
+            output.WriteLine(str2);
+            output.WriteLine("------------ ---------- --------");
+            for(int i = 0; i < 5; i++)
+            {
+                str2 = System.Convert.ToString(str[i],8);
+                str2 = str2.PadLeft(12);
+                output.Write(str2);
+
+                str2 = str[i].ToString();
+                str2 = str2.PadLeft(11);
+                output.Write(str2);
+
+                str2 = System.Convert.ToString(str[i], 16);
+                str2 = str2.PadLeft(9);
+                output.WriteLine(str2);
+            }
+        }
+
+        public static void PrintStats(StreamReader input, StreamWriter output)
+        {
+            float[] flo = new float[5];
+            string str2;
+            for (int i = 0; i < 5; i++)
+            {
+                flo[i] = float.Parse(input.ReadLine());
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                str2 = flo[i].ToString("F3");
+                str2 = str2.PadLeft(20);
+                output.WriteLine(str2);
+            }
+            float min = 10000, max = -10000, sum = 0, average = 0;
+            for(int i = 0; i< 5; i++)
+            {
+                if (flo[i] < min) min = flo[i];
+                if (flo[i] > max) max = flo[i];
+                sum += flo[i];
+            }
+            average = sum / 5;
+
+            str2 = min.ToString("F3");
+            str2 = str2.PadLeft(17);
+            output.Write("Min");
+            output.WriteLine(str2);
+
+            str2 = max.ToString("F3");
+            str2 = str2.PadLeft(17);
+            output.Write("Max");
+            output.WriteLine(str2);
+
+            str2 = sum.ToString("F3");
+            str2 = str2.PadLeft(17);
+            output.Write("Sum");
+            output.WriteLine(str2);
+
+            str2 = average.ToString("F3");
+            str2 = str2.PadLeft(13);
+            output.Write("Average");
+            output.WriteLine(str2);
+        }
+    }
+}
--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Assignment1
 {
@@ -6,7 +7,16 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // using 문에 대해서는 아직 고민하지 마세요. 나중에 배웁니다.
+            using (StreamReader READER = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter WRITER = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true })
+            {
+                Console.WriteLine("Enter your 5 digits for PrintIntegers():");
+                Assignment1.PrintIntegers(READER, WRITER);
+
+                Console.WriteLine("Enter your 5 digits for PrintStats():");
+                Assignment1.PrintStats(READER, WRITER);
+            }
         }
     }
 }
