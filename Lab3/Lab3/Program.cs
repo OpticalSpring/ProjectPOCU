2efd90aea47e407260bb312edd2c0b1e84fbee3f
OpticalSpring <54758407+OpticalSpring@users.noreply.github.com>
2019-09-17 19:42:30 +0900

    55
    55

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
 
 namespace Lab3
 {
@@ -6,7 +8,28 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string itemCostsInput = "25.34\n10.99\n250.22\n21.87\n50.24\n15";
+            string payerCountInput = "8\n";
+            string individualCostInput = "52.24\n";
+
+            byte[] buffer1 = Encoding.UTF8.GetBytes(itemCostsInput);
+            byte[] buffer2 = Encoding.UTF8.GetBytes(payerCountInput);
+            byte[] buffer3 = Encoding.UTF8.GetBytes(individualCostInput);
+
+            // using 문에 대해서는 아직 걱정하지 마세요. 나중에 배울 겁니다
+            using (StreamReader reader1 = new StreamReader(new MemoryStream(buffer1)))
+            using (StreamReader reader2 = new StreamReader(new MemoryStream(buffer2)))
+            using (StreamReader reader3 = new StreamReader(new MemoryStream(buffer3)))
+            {
+                double totalCost = RestaurantBillCalculator.CalculateTotalCost(reader1);
+                Debug.Assert(totalCost == 433.08);
+
+                double individualCost = RestaurantBillCalculator.CalculateIndividualCost(reader2, totalCost);
+                Debug.Assert(individualCost == 54.14);
+
+                uint payerCount = RestaurantBillCalculator.CalculatePayerCount(reader3, totalCost);
+                Debug.Assert(payerCount == 9);
+            }
         }
     }
 }
---
+++ Lab3/Lab3/RestaurantBillCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+
+namespace Lab3
+{
+    public static class RestaurantBillCalculator
+    {
+        public static double CalculateTotalCost(StreamReader input)
+        {
+            double[] str = new double[6];
+            double sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                str[i] = double.Parse(input.ReadLine());
+                sum += str[i];
+            }
+            str[5] = double.Parse(input.ReadLine());
+            double tax = sum * 0.05;
+            double tip = (sum + tax) * str[5] / 100;
+            double all = sum + tax + tip;
+
+
+            Console.WriteLine("음식 가격 = " + Math.Round(sum, 2));
+            Console.WriteLine("세금 = " + Math.Round(tax, 2));
+            Console.WriteLine("팁 = " + Math.Round(tip, 2));
+            Console.WriteLine("세금 및 팁까지 포함한 가격 = " + Math.Round(all,2));
+            return all;
+        }
+
+        public static double CalculateIndividualCost(StreamReader input, double totalCost)
+        {
+            int cost = int.Parse(input.ReadLine());
+            return Math.Round(totalCost / cost, 2);
+        }
+
+        public static uint CalculatePayerCount(StreamReader input, double totalCost)
+        {
+            double count = double.Parse(input.ReadLine());
+            uint a = (uint)(totalCost / count);
+            return a+1;
+        }
+    }
+}
