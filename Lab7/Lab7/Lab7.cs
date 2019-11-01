6852a64b47af015f17ea79d86b6e8eaa330c0393
cklimiteduser1 <56728663+cklimiteduser1@users.noreply.github.com>
2019-11-01 14:22:51 +0900

    asdf

---
+++ Lab7/Lab7/Lab7.cs
@@ -0,0 +1,84 @@
+﻿using System;
+namespace Lab7
+{
+    public static class Lab7
+    {
+        static int count = 0;
+        public static bool PlayGame(uint[] array)
+        {
+            count = 0;
+            if (PlayGame2(array) == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool PlayGame2(uint[] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write("{0}", array[i]);
+            }
+            Console.WriteLine();
+
+            if (count > 10)
+            {
+                Console.WriteLine("InfinityStop");
+                return false;
+            }
+            if (array.GetLength(0) < 2 || array[0] >= array.GetLength(0))
+            {
+                Console.WriteLine("Error");
+                return false;
+            }
+
+            uint shift = array[0] + array[array[0]];
+            uint shift2 = array[0] - array[array[0]];
+
+            if (shift2 > 0 && shift2 < array.GetLength(0))
+            {
+                if (array[shift2] == 0)
+                {
+                    Console.WriteLine("True");
+                    return true;
+                }
+                else
+                {
+                    count++;
+                    Console.WriteLine("LShift");
+                    uint[] newArray = array;
+                    newArray[0] = shift2;
+
+                    if (PlayGame2(newArray) == true)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (shift < array.GetLength(0))
+            {
+                if (array[shift] == 0)
+                {
+                    Console.WriteLine("True");
+                    return true;
+                }
+                else
+                {
+                    count++;
+                    Console.WriteLine("RShift");
+                    uint[] newArray = array;
+                    newArray[0] = shift;
+                    if (PlayGame2(newArray) == true)
+                    {
+                        return true;
+                    }
+                }
+            }
+            Console.WriteLine("False");
+            return false;
+        }
+    }
+}
+
+
--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Diagnostics;
 
 namespace Lab7
 {
@@ -6,7 +6,40 @@ namespace Lab7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            uint[] array = new uint[0];
+            bool bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(!bPass);
+
+            array = new uint[1] { 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(!bPass);
+
+            array = new uint[2] { 1, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(bPass);
+
+            array = new uint[4] { 2, 5, 1, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(bPass);
+
+            array = new uint[9] { 4, 3, 4, 4, 1, 5, 7, 1, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(bPass);
+
+            array = new uint[10] { 6, 1, 5, 2, 4, 8, 2, 1, 4, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(!bPass);
+
+            array = new uint[12] { 3, 6, 2, 4, 4, 6, 1, 1, 4, 5, 1, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(bPass);
         }
     }
 }
