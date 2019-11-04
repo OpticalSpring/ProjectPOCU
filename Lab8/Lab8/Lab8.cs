8455f971230c180075de8679eb33490047c282c6
cklimiteduser1 <56728663+cklimiteduser1@users.noreply.github.com>
2019-11-04 11:02:21 +0900

    Update Lab8.cs

--- Lab8/Lab8/Lab8.cs
+++ Lab8/Lab8/Lab8.cs
@@ -4,23 +4,38 @@
     {
         public static string PrettifyList(string s)
         {
+
             int n1Count = 1;
+            int n2Count = 96;
             string rString = "1) ";
-            for(int i = 0; i < s.Length; i++)
+            for (int i = 0; i < s.Length; i++)
             {
-                if(s[i] == '|')
+                if (s[i] == '|')
                 {
                     n1Count++;
-                    rString += "\n";
+                    n2Count = 96;
+                    rString += "\r\n";
                     rString += n1Count;
                     rString += ") ";
+                }
+                else if (s[i] == '_')
+                {
+                    n2Count++;
+                    rString += "\r\n    ";
+                    rString += (char)n2Count;
+                    rString += ") ";
+                }
+                else if (s[i] == '/')
+                {
+                    rString += "\r\n        ";
+                    rString += "- ";
                 }
                 else
                 {
                     rString += s[i];
                 }
             }
-            rString += "\n";
+            rString += "\r\n";
             return rString;
         }
     }
