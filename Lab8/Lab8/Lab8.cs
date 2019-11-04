95d81d6fd049c7634f582ea6801b9e6117d006fa
cklimiteduser1 <56728663+cklimiteduser1@users.noreply.github.com>
2019-11-04 11:18:10 +0900

    asd

--- Lab8/Lab8/Lab8.cs
+++ Lab8/Lab8/Lab8.cs
@@ -4,13 +4,19 @@
     {
         public static string PrettifyList(string s)
         {
-            for (int i = 0; i < 1; i++)
+            if(s == null)
             {
-                if(s[i] == 0 || s[i] == 32 || s[i] == 0x9 || s[i] == 0xa || s[i] == 0xb || s[i] == 0xc || s[i] == 0xd || s[i] == 0x20)
-                {
-                    return null;
-                }
+                return null;
             }
+            else if (s.Length == 0)
+            {
+                return null;
+            }
+            else if (s[0] == 0 || s[0] == 32 || s[0] == 0x9 || s[0] == 0xa || s[0] == 0xb || s[0] == 0xc || s[0] == 0xd || s[0] == 0x20)
+            {
+                return null;
+            }
+
             int n1Count = 1;
             int n2Count = 96;
             string rString = "1) ";
--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -7,55 +7,21 @@ namespace Lab8
     {
         static void Main(string[] args)
         {
-            string minifiedList = "Apple|Orange|Banana";
-            string prettifiedList = @"1) Apple
-2) Orange
-3) Banana
-";
+            string minifiedList = " ";
             string list = Lab8.PrettifyList(minifiedList);
-            Console.WriteLine(list);
-            Debug.Assert(prettifiedList == list);
+            Debug.Assert(null == list);
 
-            Console.WriteLine("---------------------------------");
-
-            minifiedList = "Apple_Fuji_Gala|Orange|Banana_Baby_Cavendish";
-            prettifiedList = @"1) Apple
-    a) Fuji
-    b) Gala
-2) Orange
-3) Banana
-    a) Baby
-    b) Cavendish
-";
-
+            minifiedList = "";
             list = Lab8.PrettifyList(minifiedList);
-            Console.WriteLine(list);
-            Debug.Assert(prettifiedList == list);
+            Debug.Assert(null == list);
 
-            Console.WriteLine("---------------------------------");
-
-            minifiedList = "Week 1_Course Explanation/it's fun/it's awesome_Hello World_Types of Programming Languages|Week 2_Console output_Variables_Primitive Types|Week 3_Casting_Operator_String_Console input";
-            prettifiedList = @"1) Week 1
-    a) Course Explanation
-        - it's fun
-        - it's awesome
-    b) Hello World
-    c) Types of Programming Languages
-2) Week 2
-    a) Console output
-    b) Variables
-    c) Primitive Types
-3) Week 3
-    a) Casting
-    b) Operator
-    c) String
-    d) Console input
-";
-
+            minifiedList = "             ";
             list = Lab8.PrettifyList(minifiedList);
-            Console.WriteLine(list);
-            Debug.Assert(prettifiedList == list);
+            Debug.Assert(null == list);
 
+            minifiedList = null;
+            list = Lab8.PrettifyList(minifiedList);
+            Debug.Assert(null == list);
         }
     }
 }
