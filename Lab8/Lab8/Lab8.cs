00ac2391f6d7b11471d1eee729c96ed26d297a66
cklimiteduser1 <56728663+cklimiteduser1@users.noreply.github.com>
2019-11-04 11:09:52 +0900

    Update Lab8.cs

--- Lab8/Lab8/Lab8.cs
+++ Lab8/Lab8/Lab8.cs
@@ -4,7 +4,13 @@
     {
         public static string PrettifyList(string s)
         {
-
+            for (int i = 0; i < 1; i++)
+            {
+                if(s[i] == 0 || s[i] == 32 || s[i] == 0x9 || s[i] == 0xa || s[i] == 0xb || s[i] == 0xc || s[i] == 0xd || s[i] == 0x20)
+                {
+                    return null;
+                }
+            }
             int n1Count = 1;
             int n2Count = 96;
             string rString = "1) ";
