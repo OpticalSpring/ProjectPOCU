c8476e6abf7e3c7266106fe6a5ea73082a44195b
cklimiteduser1 <56728663+cklimiteduser1@users.noreply.github.com>
2019-11-04 10:38:48 +0900

    ;

---
+++ Lab8/Lab8/Lab8.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab8
+{
+    public static class Lab8
+    {
+        public static string PrettifyList(string s)
+        {
+            int n1Count = 1;
+            string rString = "1) ";
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(s[i] == '|')
+                {
+                    n1Count++;
+                    rString += "\n";
+                    rString += n1Count;
+                    rString += ") ";
+                }
+                else
+                {
+                    rString += s[i];
+                }
+            }
+            rString += "\n";
+            return rString;
+        }
+    }
+}
--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Lab8
 {
@@ -6,7 +7,55 @@ namespace Lab8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string minifiedList = "Apple|Orange|Banana";
+            string prettifiedList = @"1) Apple
+2) Orange
+3) Banana
+";
+            string list = Lab8.PrettifyList(minifiedList);
+            Console.WriteLine(list);
+            Debug.Assert(prettifiedList == list);
+
+            Console.WriteLine("---------------------------------");
+
+            minifiedList = "Apple_Fuji_Gala|Orange|Banana_Baby_Cavendish";
+            prettifiedList = @"1) Apple
+    a) Fuji
+    b) Gala
+2) Orange
+3) Banana
+    a) Baby
+    b) Cavendish
+";
+
+            list = Lab8.PrettifyList(minifiedList);
+            Console.WriteLine(list);
+            Debug.Assert(prettifiedList == list);
+
+            Console.WriteLine("---------------------------------");
+
+            minifiedList = "Week 1_Course Explanation/it's fun/it's awesome_Hello World_Types of Programming Languages|Week 2_Console output_Variables_Primitive Types|Week 3_Casting_Operator_String_Console input";
+            prettifiedList = @"1) Week 1
+    a) Course Explanation
+        - it's fun
+        - it's awesome
+    b) Hello World
+    c) Types of Programming Languages
+2) Week 2
+    a) Console output
+    b) Variables
+    c) Primitive Types
+3) Week 3
+    a) Casting
+    b) Operator
+    c) String
+    d) Console input
+";
+
+            list = Lab8.PrettifyList(minifiedList);
+            Console.WriteLine(list);
+            Debug.Assert(prettifiedList == list);
+
         }
     }
 }
