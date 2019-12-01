c9f2a1067f547a8991f8b4299039d0ace55483ea
cklimiteduser1 <56728663+cklimiteduser1@users.noreply.github.com>
2019-12-01 14:53:08 +0900

    k

---
+++ Lab11/Lab11/Encoder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace Lab11
+{
+    public static class Encoder
+    {
+        public static bool TryEncode(Stream input, Stream output)
+        {
+            if(input.Length == 0 || input == null)
+            {
+                return false;
+            }
+            string str = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                str += input.ReadByte();
+               if(str[i] == ' ')
+                {
+                    
+                    str = str.Substring(0,str.Length - 1);
+                    str += '0';
+                }
+            }
+            str += "00";
+            Console.WriteLine(str);
+            string rStr = "";
+            string b = ""+str[0] + str[1];
+            int count = 0;
+            int charValue = int.Parse(b);
+            for (int i = 0; i < str.Length-1; i+=2)
+            {
+                string a = "" + str[i] + str[i + 1];
+                if(charValue != int.Parse(a))
+                {
+                    rStr += Convert.ToString(count, 16).PadLeft(2, '0') + " " + Convert.ToString(charValue,16).PadLeft(2,'0') + " ";
+                    count = 1;
+                    charValue = int.Parse(a);
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    count++;
+                }
+
+            }
+            Console.WriteLine(rStr);
+            return true;
+        }
+
+        public static bool TryDecode(Stream input, Stream output)
+        {
+            return false;
+        }
+    }
+}
--- Lab11/Lab11/Program.cs
+++ Lab11/Lab11/Program.cs
@@ -1,12 +1,100 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace Lab11
 {
     class Program
     {
+        private const string ENCODE_TEST1 = "encode1.txt";
+        private const string ENCODE_TEST2 = "encode2.txt";
+
+        private const string DECODE_TEST1 = "decode1.bin";
+        private const string DECODE_TEST2 = "decode2.bin";
+
+        private const string EMPTY_ENCODE_TEST = "empty_encode.txt";
+        private const string EMPTY_DECODE_TEST = "empty_decode.bin";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            setup();
+
+            using (Stream read = File.OpenRead(ENCODE_TEST1))
+            using (Stream write = File.OpenWrite($"{ENCODE_TEST1}.output.bin"))
+            {
+                bool bEncoded = Encoder.TryEncode(read, write);
+                Debug.Assert(bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(DECODE_TEST1))
+            using (Stream write = File.OpenWrite($"{DECODE_TEST1}.output.txt"))
+            {
+                bool bEncoded = Encoder.TryDecode(read, write);
+                // Check decode1.bin.output.txt. The content should be the same as encode1.txt
+                Debug.Assert(bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(ENCODE_TEST2))
+            using (Stream write = File.OpenWrite($"{ENCODE_TEST2}.output.bin"))
+            {
+                bool bEncoded = Encoder.TryEncode(read, write);
+                Debug.Assert(bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(DECODE_TEST2))
+            using (Stream write = File.OpenWrite($"{DECODE_TEST2}.output.txt"))
+            {
+                bool bEncoded = Encoder.TryDecode(read, write);
+                // Check decode2.bin.output.txt. The content should be the same as encode2.txt
+                Debug.Assert(bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(EMPTY_ENCODE_TEST))
+            using (Stream write = File.OpenWrite($"{EMPTY_ENCODE_TEST}.output.bin"))
+            {
+                bool bEncoded = Encoder.TryEncode(read, write);
+                Debug.Assert(!bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(EMPTY_DECODE_TEST))
+            using (Stream write = File.OpenWrite($"{EMPTY_DECODE_TEST}.output.txt"))
+            {
+                bool bEncoded = Encoder.TryDecode(read, write);
+                // Check empty_decode.bin.output.txt. The content should be empty.
+                Debug.Assert(!bEncoded);
+            }
+        }
+
+        private static void setup()
+        {
+            if (File.Exists($"{ENCODE_TEST1}.output.bin"))
+            {
+                File.Delete($"{ENCODE_TEST1}.output.bin");
+            }
+
+            if (File.Exists($"{DECODE_TEST1}.output.txt"))
+            {
+                File.Delete($"{DECODE_TEST1}.output.txt");
+            }
+
+            if (File.Exists($"{ENCODE_TEST2}.output.bin"))
+            {
+                File.Delete($"{ENCODE_TEST2}.output.bin");
+            }
+
+            if (File.Exists($"{DECODE_TEST2}.output.txt"))
+            {
+                File.Delete($"{DECODE_TEST2}.output.txt");
+            }
+
+            if (File.Exists($"{EMPTY_ENCODE_TEST}.output.bin"))
+            {
+                File.Delete($"{EMPTY_ENCODE_TEST}.output.bin");
+            }
+
+            if (File.Exists($"{EMPTY_DECODE_TEST}.output.txt"))
+            {
+                File.Delete($"{EMPTY_DECODE_TEST}.output.txt");
+            }
         }
     }
 }
