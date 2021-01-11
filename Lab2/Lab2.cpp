85c450a2810df8237adba62b175ffb0245703290
OpticalSpring <cosmos5477@gmail.com>
2021-01-11 23:48:15 +0900

    no message

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,51 @@
+#include "Lab2.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+using namespace std;
+namespace lab2
+{
+    void PrintIntegers(std::istream& in, std::ostream& out)
+    {
+		vector<int> v;
+
+
+		int number;
+		int sum = 0;
+
+		while (true)
+		{
+			cin >> number;
+			if (cin.eof())
+			{
+				break;
+			}
+
+			if (cin.fail())
+			{
+				cout << "Invalid input" << endl;
+				cin.clear();
+				cin.ignore(LLONG_MAX, '\n');
+				continue;
+			}
+			v.push_back(number);
+		}
+		cin.clear();
+
+		
+		out << "         oct        dec      hex" << endl;
+		out << "------------ ---------- --------" << endl;
+		for (size_t i = 0; i < v.size(); i++)
+		{
+			out <<uppercase <<setfill(' ') << setw(12) <<oct << v[i] ;
+			out << setw(11) << dec << v[i];
+			out << setw(9) << hex << v[i] << endl;
+		}
+    }
+
+    void PrintMaxFloat(std::istream& in, std::ostream& out)
+    {
+
+    }
+}
