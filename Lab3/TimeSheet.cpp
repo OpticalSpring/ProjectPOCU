b8a88fda3dff925f262709a75d432ea3994b43d8
OpticalSpring <cosmos5477@gmail.com>
2021-01-18 21:50:31 +0900

    no message

--- Lab3/TimeSheet.cpp
+++ Lab3/TimeSheet.cpp
@@ -2,72 +2,72 @@
 
 namespace lab3
 {
-    TimeSheet::TimeSheet(const char* name, unsigned int maxEntries)
-    {
-        mSheetName = new char[strlen(name)+1];
-        strcpy(mSheetName, name);
-        mSheetTime = new int[maxEntries];
-        mSheetTimeLength = maxEntries;
-        mSheetTimeNow = 0;
-    }
+	TimeSheet::TimeSheet(const char* name, unsigned int maxEntries)
+	{
+		mSheetName = new char[strlen(name) + 1];
+		strcpy(mSheetName, name);
+		mSheetTime = new int[maxEntries];
+		mSheetTimeLength = maxEntries;
+		mSheetTimeNow = 0;
+	}
 
-    void TimeSheet::AddTime(int timeInHours)
-    {
-        if (timeInHours < 1 || timeInHours > 10) 
-        {
-            return;
-        }
-        if (mSheetTimeNow > mSheetTimeLength) 
-        {
-            return;
-        }
-        mSheetTime[mSheetTimeNow] = timeInHours;
-        mSheetTimeNow++;
-    }
+	void TimeSheet::AddTime(int timeInHours)
+	{
+		if (timeInHours < 1 || timeInHours > 10)
+		{
+			return;
+		}
+		if (mSheetTimeNow > mSheetTimeLength)
+		{
+			return;
+		}
+		mSheetTime[mSheetTimeNow] = timeInHours;
+		mSheetTimeNow++;
+	}
 
-    int TimeSheet::GetTimeEntry(unsigned int index) const
-    {
-        if (index < 0 || index > mSheetTimeLength - 1) 
-        {
-            return -1;
-        }
-        else 
-        {
-            return mSheetTime[index];
-        }
-    }
+	int TimeSheet::GetTimeEntry(unsigned int index) const
+	{
+		if (index < 0 || index > mSheetTimeLength - 1)
+		{
+			return -1;
+		}
+		else
+		{
+			return mSheetTime[index];
+		}
+	}
 
-    int TimeSheet::GetTotalTime() const
-    {
-        int sum = 0;
-        for (size_t i = 0; i < mSheetTimeLength; i++)
-        {
-            sum += mSheetTime[i];
-        }
-        return sum;
-    }
+	int TimeSheet::GetTotalTime() const
+	{
+		int sum = 0;
+		for (size_t i = 0; i < mSheetTimeLength; i++)
+		{
+			sum += mSheetTime[i];
+		}
+		return sum;
+	}
 
-    float TimeSheet::GetAverageTime() const
-    {
-        float avg = GetTotalTime()/mSheetTimeLength;
-        return avg;
-    }
+	float TimeSheet::GetAverageTime() const
+	{
+		float avg = GetTotalTime() / mSheetTimeLength;
+		return avg;
+	}
 
-    float TimeSheet::GetStandardDeviation() const
-    {
-        float avg = GetAverageTime();
-        float var = 0;
-        for (size_t i = 0; i < mSheetTimeLength; i++)
-        {
-            var += pow(mSheetTime[i] - avg, 2);
-        }
-        var = sqrt(var);
+	float TimeSheet::GetStandardDeviation() const
+	{
+		float avg = GetAverageTime();
+		float var = 0;
+		for (size_t i = 0; i < mSheetTimeLength; i++)
+		{
+			var += pow(mSheetTime[i] - avg, 2);
+		}
+		var = sqrt(var);
 
-        return var;
-    }
+		return var;
+	}
 
-    const std::string& TimeSheet::GetName() const
-    {
-        return mSheetName;
-    }
+	const std::string& TimeSheet::GetName() const
+	{
+		return mSheetName;
+	}
 }
--- Lab3/TimeSheet.h
+++ Lab3/TimeSheet.h
@@ -3,22 +3,21 @@
 
 namespace lab3
 {
-    class TimeSheet
-    {
-    public:
-        TimeSheet(const char* name, unsigned int maxEntries);
-        void AddTime(int timeInHours);
-        int GetTimeEntry(unsigned int index) const;
-        int GetTotalTime() const;
-        float GetAverageTime() const;
-        float GetStandardDeviation() const;
-        const std::string& GetName() const;
+	class TimeSheet
+	{
+	public:
+		TimeSheet(const char* name, unsigned int maxEntries);
+		void AddTime(int timeInHours);
+		int GetTimeEntry(unsigned int index) const;
+		int GetTotalTime() const;
+		float GetAverageTime() const;
+		float GetStandardDeviation() const;
+		const std::string& GetName() const;
 
-    private:
-        char* mSheetName;
-        int* mSheetTime;
-        int mSheetTimeLength;
-        int mSheetTimeNow;
-        // 필요에 따라 private 변수와 메서드를 추가하세요.
-    };
+	private:
+		char* mSheetName;
+		int* mSheetTime;
+		int mSheetTimeLength;
+		int mSheetTimeNow;
+	};
 }
