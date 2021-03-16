7d0768212d575e82a8f6242dcf1fad3ae1cf10bc
OpticalSpring <cosmos5477@gmail.com>
2021-03-16 22:46:12 +0900

    no message

---
+++ Lab9/Game.cpp
@@ -0,0 +1,56 @@
+#include "Game.h"
+
+
+namespace lab9
+{
+	Game::Game(unsigned int seed, unsigned int poolSize) :mObjectPool(poolSize)
+	{
+		
+		srand(seed);
+	}
+
+	Game::~Game()
+	{
+		for (auto it = mActiveGameObjects.begin(); it != mActiveGameObjects.end(); ++it)
+		{
+			delete (*it);
+		}
+
+		mActiveGameObjects.clear();
+	}
+
+	void Game::Spawn()
+	{
+		IceCube* iceCube = new IceCube();
+		iceCube->Initialize(rand() % MAX_FRAME_COUNT_TO_LIVE + 1);
+		mActiveGameObjects.push_back(iceCube);
+	}
+
+	void Game::Update()
+	{
+		for (auto it = mActiveGameObjects.begin(); it != mActiveGameObjects.end();)
+		{
+			IceCube* iceCube = *it;
+			iceCube->Animate();
+
+			if (!iceCube->IsActive())
+			{
+				it = mActiveGameObjects.erase(it);
+				delete iceCube;
+				continue;
+			}
+
+			++it;
+		}
+	}
+
+	const std::vector<IceCube*>& Game::GetActiveGameObjects() const
+	{
+		return mActiveGameObjects;
+	}
+
+	ObjectPool<IceCube>& Game::GetObjectPool() 
+	{
+		return mObjectPool;
+	}
+}
---
+++ Lab9/ObjectPool.h
@@ -0,0 +1,52 @@
+#pragma once
+#include<queue>
+using namespace std;
+namespace lab9
+{
+	template<typename T>
+	class ObjectPool 
+	{
+	public:
+		ObjectPool<T>(size_t poolSize);
+		T* Get();
+		void Return(T* t);
+		size_t GetFreeObjectCount();
+		size_t GetMaxFreeObjectCount();
+
+	private:
+		size_t maxPoolSize;
+		queue<T*> poolObject;
+	};
+	template<typename T>
+	inline ObjectPool<T>::ObjectPool(size_t poolSize)
+	{
+		maxPoolSize = poolSize;
+	}
+	template<typename T>
+	inline T* ObjectPool<T>::Get()
+	{
+		auto rValue = poolObject.front();
+			poolObject.pop();
+		return rValue;
+	}
+	template<typename T>
+	inline void ObjectPool<T>::Return(T* t)
+	{
+		if (poolObject.size() == maxPoolSize) 
+		{
+			delete t;
+			return;
+		}
+		poolObject.push(t);
+	}
+	template<typename T>
+	inline size_t ObjectPool<T>::GetFreeObjectCount()
+	{
+		return poolObject.size();
+	}
+	template<typename T>
+	inline size_t ObjectPool<T>::GetMaxFreeObjectCount()
+	{
+		return maxPoolSize;
+	}
+}
